c7d58f4197e939def37c206ea2fa35e2bb60ada3
Elnifio <elnifio0519@gmail.com>
2021-07-30 14:48:38 +0800

    update Table.js

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="table">
-        <table aria-label="Main Data Table">
-            <th id="DateContainer">
-                <td is="TableHeader" :content="'123'"></td>
-            </th>
-            <tr is="TableRow">
-                <td is="TableEntry"></td>
-            </tr>
-        </table>
+        
     </div>
 </template>
 
 <script>
+import Table from "../utils/Table.js";
 
 export default {
     name: "Table",
+    props: {
+
+    },
+    data() {
+        return {}
+    }
 }
 </script>
--- src/utils/Table.js
+++ src/utils/Table.js
@@ -1,11 +1,26 @@
+import Vue from "vue";
+
 class Table {
     /**
      * 
-     * @param {String} zipped zipped Table storage string
+     * @param {zipped} zipped zipped Table storage string
      */
-    constructor(name, words) {
+    constructor(name, words, showLast=5) {
         this.name = name;
         this.words = words;
+        this.showLast = showLast;
+    }
+
+    get dates() {
+        return Array.sort(this.words.reduce((accu, curr) => 
+            accu.concat(curr.dates.filter(x => accu.indexOf(x) < 0)), []));
+    }
+
+    // TODO: Finish this: a record of all table entries
+    get records() {
+        return this.words.reduce((accu, curr) => {
+            return 0;
+        })
     }
 
     zip() {
@@ -18,38 +33,40 @@ class Table {
  * @param {Object[]} zipped 
  */
 function unzipUnit(zipped) {
-    return new Unit(zipped[0], zipped[1], zipped[2]);
+    return new Unit(zipped[0], zipped[1], zipped[2], zipped[3]);
 }
 
 class Unit {
-    constructor(spelling, highlight="white", records = []) {
+    constructor(spelling, highlight="white", records={}, annot="") {
         this.spelling = spelling;
         this.highlight = highlight;
         this.records = records;
+        this.annot = annot;
     }
 
     zip() {
-        return [this.spelling, this.highlight, this.records];
+        return [this.spelling, this.highlight, this.records, this.annot];
     }
 
     add(entry) {
-        this.records.push(entry);
+        if (!this.records[entry]) {
+            this.records[entry] = 0;
+        }
+        this.records[entry] += 1;
     }
 
     delete(entry) {
-        let idx = this.records.indexOf(entry);
-        if (idx >= 0) {
-            this.records.splice(idx, 1);
+        if (this.records[entry] > 0) {
+            this.records[entry] -= 1;
+        }
+
+        if (this.records[entry] <= 0) {
+            Vue.delete(this.records, entry);
         }
     }
 
     get dates() {
-        return this.records.reduce((accu, curr) => {
-            if (!accu[curr]) {
-                accu[curr] = 0;
-            }
-                
-        })
+        return Object.keys(this.records);
     }
 }
 
